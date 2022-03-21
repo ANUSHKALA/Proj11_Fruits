44e2a1b126768443543a5d5d1afd2ee4bb5a8b9f
ANUSHKALA <anushkakaimal2003@gmail.com>
2022-03-21 19:52:07 +0530

    mongoose for storing fruits data

--- app.js
+++ app.js
@@ -1,59 +1,37 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'fruitsDB';
-
-// Create a new MongoClient
-const client = new MongoClient(url);
+const mongoose = require('mongoose')
 
 
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+mongoose.connect("mongodb://localhost:27017/fruitsDB");
 
-  const db = client.db(dbName);
-
-
-  // db.collection('numbers').insertMany(
-  //   [
-  //    {
-  //      name:"apple",
-  //      reting:"3",
-  //      review:"i hate apples",
-  //     },
-  //     {
-  //       name:"banana",
-  //       reting:"8",
-  //       review:"not bad",
-  //      },
-  //      {
-  //       name:"mango",
-  //       reting:"2",
-  //       review:"gives pimples",
-  //      },
-  //   ], 
-  //   function(err, r) {
-  //   assert.equal(null, err);
-  //   assert.equal(3, r.insertedCount);
-  //   
-  //   client.close();
-  // });
-
-
-    insertDocuments(db,function (){
-      client.close();
-    })
-
-    findDocuments(db,function(){
-      client.close();
-    })
-
+// like constructor => the data will be arranged in the following form only
+const fruitSchema = new mongoose.Schema({
+  name:String,
+  rating:Number,
+  review:String
 });
+
+/*
+creating a model  
+mentioning the collection as singular; mongoose will create a collection with plural name
+mongoose actually used lodash to achieve this
+also mentioning the schema for the data
+*/
+
+const Fruit = mongoose.model("Fruit",fruitSchema);
+
+const fruit = new Fruit({
+  name:"Apple",
+  rating:3,
+  review:"I hate apples."
+});
+
+/* 
+To save
+=> fruit document
+=> Fruit collection
+=> inside fruitsDB
+*/
+fruit.save();
 
 const insertDocuments = function (db,callback){
 
